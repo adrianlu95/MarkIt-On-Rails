10b179c373937db8089345ab590120a35238a46c
adrianlu95 <alu5@lion.lmu.edu>
2017-02-09 16:19:58 -0800

    added some tests that dont exist yet, and starting to make users pages

--- MarkIt/config/routes.rb
+++ MarkIt/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  root 'application#hello'
+  get 'users/new'
+  get 'static_pages/home'
+  get  '/signup',  to: 'users#new'
 end
