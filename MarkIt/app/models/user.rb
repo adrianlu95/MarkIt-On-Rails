87652599985f380f909f8a0393d3333210a017c5
adrianlu95 <alu5@lion.lmu.edu>
2017-03-20 17:14:22 -0700

    fixing a few small errors hoping this fixes things

--- MarkIt/app/models/user.rb
+++ MarkIt/app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   validates :firstname, presence: true, length: {maximum: 50}
   validates :lastname, presence: true, length: {maximum: 50}
   validates :username, presence: true, length: {maximum: 50}
-  validates :username, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
 end
